360323d2c3dffa5b0e179cb6c6591cc93fe78a51
DanielAngel2003 <daniel.angel_2003@outlook.com>
2025-05-27 09:43:12 -0600

    Implementación de keyword_await()
    El código ahora espera a una palabra clave para poder activarsze. Mientras tanto, escucha e imprime lo que escucha.
    De momento, parece que solamente es atento a cuando se habla en español. Sería interesante poder habilitarlo en inglés

--- voice_testing.py
+++ voice_testing.py
@@ -26,14 +26,44 @@ def escuchar():
         except sr.RequestError:
             return 'Error con reconocimiento'
 
-# Ejemplo de funcionamiento
-hablar("Hola, ¿en qué puedo ayudarte?")
-comando = escuchar()
-if "hora" in comando:
-    hora = datetime.datetime.now().strftime("%H:%M")
-    hablar(f"Son las {hora}")
-elif comando: 
-    hablar('Perdona, aún no tengo un comando para eso')
-else:
-    hablar('No escuché ningún comando.')
+def active():
+    # Ejemplo de funcionamiento
+    hablar("Hola, ¿en qué puedo ayudarte?")
+    comando = escuchar()
+    if "hora" in comando:
+        hora = datetime.datetime.now().strftime("%H:%M")
+        hablar(f"Son las {hora}")
+    elif comando: 
+        hablar('Perdona, aún no tengo un comando para eso')
+    else:
+        hablar('No escuché ningún comando.')
 
+def keyword_await(keyword='asistente'):
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Esperando Palabra Clave...')
+        while True:
+            try:
+                audio = recognizer.listen(source, timeout=5)
+                texto = recognizer.recognize_google(audio, language='es-ES').lower()
+                print(f"👂 Escuchado: {texto}")
+                if keyword in texto:
+                    print("✅ Palabra clave detectada.")
+                    active()
+                    print("Esperando nuevamente...")
+            except sr.WaitTimeoutError:
+                pass  # silencio prolongado, se ignora
+            except sr.UnknownValueError:
+                pass  # ruido o no se entendió
+            except sr.RequestError:
+                print("❌ Error con la API de reconocimiento.")
+                break
+
+keyword_await()
+
+"""
+26/05/2025:
+Prueba principal lograda, devuelve la hora [20:32]
+
+Ahora veré como puedo implementar cada vez más opciones dentro del modelo, tomanod esta vase que ahora tengo.
+"""
