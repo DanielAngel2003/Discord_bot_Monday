2c0297b59116e2db5503a111cbae6e17d86e671c
DanielAngel2003 <daniel.angel_2003@outlook.com>
2025-05-29 15:09:21 -0600

    Limpieza de código

--- monday_core.py
+++ monday_core.py
@@ -22,7 +22,9 @@ class CoreCog(commands.Cog):
         self.bot = bot
         # Propiedad para mantenerse activo
         self.bot.bot_on = True
+        # Nombres para responder
         self.calls = ['oye monday', 'monday', 'hey monday', 'oye, monday', 'hey, monday', 'disculpa, monday']
+        # Palabras que detecta en el mensaje para eliminarlo
         self.banned_words = ['nigger', 'nigga', 'prieto']
 
     ### Setting in the bot
@@ -97,20 +99,29 @@ class CoreCog(commands.Cog):
         # Salutes back the person that saluted M.O.N.D.A.Y.
         await ctx.channel.send(f'Hello, {ctx.author.name}!')
 
+    ### Command for Turning On
     @commands.command()
     async def on(self, ctx):
+        # Command to detect the 'On' command for M.O.N.D.A.Y.
+        # Allows M.O.N.D.A.Y. to act as it is programmed
         self.bot.bot_on = True
         await ctx.send('M.O.N.D.A.Y. On')
 
+    ### Command for Turning Off
     @commands.command()
     async def off(self, ctx):
+        #Command to detect the 'Off' for M.O.N.D.A.Y.
         self.bot.bot_on = False
         await ctx.send('M.O.N.D.A.Y. Off')
     
+    ### Command for Shuting down
     @commands.command()
     async def sleep(self,ctx):
+        #Command to detect the 'sleep' for M.O.N.D.A.Y.
         await ctx.send('See you all when September ends.')
+        #Shuts down the bot entirely
         await self.bot.close()
 
+# Permite que se puedan utilizar como Cogs para un bot de Discord
 async def setup(bot):
     await bot.add_cog(CoreCog(bot))
